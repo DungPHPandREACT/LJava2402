7a92a29117eb1c794564d74d3949e524d0b33df9
DungPHPAndReact <dung2000cn@gmail.com>
2024-03-31 10:22:01 +0700

    bt3

--- Lesson11/homework_dom/scripts.js
+++ Lesson11/homework_dom/scripts.js
@@ -1,33 +1,45 @@
-const $url = document.querySelector('#url');
-const $width = document.querySelector('#width');
-const $height = document.getElementById('height');
-const $radius = document.querySelector('#radius');
-const $alt = document.querySelector('#alt');
+// Bài tập 1
 
-const $image = document.querySelector('img');
-// Change url
-$url.onchange = function () {
-	const newValueURL = $url.value;
-	$image.src = newValueURL;
-};
-// Change width
-$width.oninput = function () {
-	const newValueWidth = $width.value;
-	$image.style.width = newValueWidth + 'px';
-};
-// Change height
-$height.oninput = function () {
-	const newValueHeight = $height.value;
-	$image.style.height = newValueHeight + 'px';
-};
-// Change radius
-$radius.oninput = function () {
-	const newValueRadius = $radius.value;
-	$image.style.borderRadius = newValueRadius + 'px';
+// const $url = document.querySelector('#url');
+// const $width = document.querySelector('#width');
+// const $height = document.getElementById('height');
+// const $radius = document.querySelector('#radius');
+// const $alt = document.querySelector('#alt');
+
+// const $image = document.querySelector('img');
+// // Change url
+// $url.onchange = function () {
+// 	const newValueURL = $url.value;
+// 	$image.src = newValueURL;
+// };
+// // Change width
+// $width.oninput = function () {
+// 	const newValueWidth = $width.value;
+// 	$image.style.width = newValueWidth + 'px';
+// };
+// // Change height
+// $height.oninput = function () {
+// 	const newValueHeight = $height.value;
+// 	$image.style.height = newValueHeight + 'px';
+// };
+// // Change radius
+// $radius.oninput = function () {
+// 	const newValueRadius = $radius.value;
+// 	$image.style.borderRadius = newValueRadius + 'px';
+// }
+// // Change alt
+// $alt.oninput = function () {
+// 	const newValueAlt = $alt.value;
+// 	// $image.setAttribute('alt', newValueAlt);
+//     $image.alt = newValueAlt;
+// };
+
+// Bài tập 3
+const listButton = document.querySelectorAll('td button');
+console.log(listButton);
+
+for (let button of listButton) {
+	button.onclick = function () {
+		button.parentElement.parentElement.remove();
+	};
 }
-// Change alt
-$alt.oninput = function () {
-	const newValueAlt = $alt.value;
-	// $image.setAttribute('alt', newValueAlt);
-    $image.alt = newValueAlt;
-};
