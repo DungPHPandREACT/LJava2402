c5f7394ccb7ff3bc31d4929673e3bb398b78d0c5
DungPHPAndReact <dung2000cn@gmail.com>
2024-03-31 10:02:21 +0700

    bt1

---
+++ Lesson11/homework_dom/scripts.js
@@ -0,0 +1,33 @@
+const $url = document.querySelector('#url');
+const $width = document.querySelector('#width');
+const $height = document.getElementById('height');
+const $radius = document.querySelector('#radius');
+const $alt = document.querySelector('#alt');
+
+const $image = document.querySelector('img');
+// Change url
+$url.onchange = function () {
+	const newValueURL = $url.value;
+	$image.src = newValueURL;
+};
+// Change width
+$width.oninput = function () {
+	const newValueWidth = $width.value;
+	$image.style.width = newValueWidth + 'px';
+};
+// Change height
+$height.oninput = function () {
+	const newValueHeight = $height.value;
+	$image.style.height = newValueHeight + 'px';
+};
+// Change radius
+$radius.oninput = function () {
+	const newValueRadius = $radius.value;
+	$image.style.borderRadius = newValueRadius + 'px';
+}
+// Change alt
+$alt.oninput = function () {
+	const newValueAlt = $alt.value;
+	// $image.setAttribute('alt', newValueAlt);
+    $image.alt = newValueAlt;
+};
