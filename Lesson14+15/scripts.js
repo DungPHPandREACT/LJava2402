e75b7cbf5cfe3a1ab8f35feb9930f9b9677be695
DungPHPAndReact <dung2000cn@gmail.com>
2024-04-21 09:53:41 +0700

    update student

--- Lesson14+15/scripts.js
+++ Lesson14+15/scripts.js
@@ -20,6 +20,8 @@ const listStudent = [
 const $table = document.getElementById('table');
 // Selector đến button "Create student"
 const $buttonCreate = document.getElementById('create');
+// Selector đến button "Update student"
+const $buttonUpdate = document.getElementById('update');
 // Selector đến input nhập id
 const $id = document.getElementById('id');
 // Selector đến input nhập name
@@ -113,18 +115,57 @@ const deleteStudent = (id) => {
 };
 
 // Chức năng update
+// *Hiển thị thông tin học sinh lên các ô input
 // Bước 1: Gán được sự kiện onclick cho button update
-const updateStudent = (id) => {
+const updateStudent = (idStudentUpdate) => {
 	// Bước 2: Lấy được thông tin học sinh mà mình muốn update
+	let index = -1;
+	for (let i = 0; i < listStudent.length; i++) {
+		if (listStudent[i].id === idStudentUpdate) {
+			index = i;
+		}
+	}
+	const { id, name, gender, math_score, english_score, literature_score } =
+		listStudent[index];
+	// Bước 3: Hiển thị thông tin của học sinh đó lên các ô input tương ứng
+	$id.value = id;
+	$name.value = name;
+	$gender.value = gender;
+	$math_score.value = math_score;
+	$english_score.value = english_score;
+	$literature_score.value = literature_score;
+};
+// *Cập nhật lại thông tin học sinh
+// Bước 1: gán event on click cho button "Update student"
+$buttonUpdate.onclick = () => {
+	// Bước 2: lấy toàn bộ thông tin học sinh vừa được chỉnh sửa
+	const id = Number($id.value);
+	const name = $name.value;
+	const gender = $gender.value;
+	const math_score = $math_score.value;
+	const english_score = $english_score.value;
+	const literature_score = $literature_score.value;
+
+	const studentUpdate = {
+		id,
+		name,
+		gender,
+		math_score,
+		english_score,
+		literature_score,
+	};
+	// Bước 3: tìm vị trí index của học sinh đó
 	let index = -1;
 	for (let i = 0; i < listStudent.length; i++) {
 		if (listStudent[i].id === id) {
 			index = i;
 		}
 	}
-	const studentUpdate = listStudent[index];
-	console.log('studentUpdate: ', studentUpdate);
-	// Bước 3: Hiển thị thông tin của học sinh đó lên các ô input tương ứng
+	console.log('index: ', index);
+	// Bước 4: cập nhật thông tin học sinh vào trong mảng
+	listStudent[index] = studentUpdate;
+	// Bước 5: gọi lại hàm renderStudents
+	renderStudents();
 }
 
 
